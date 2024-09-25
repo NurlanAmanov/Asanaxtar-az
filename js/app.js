713170f7be784518f6b6442a147751e430358f4b
unknown <nurlanemenov14@gmail.com>
2024-09-26 00:43:31 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const sldierbar =document.getElementById('sldierbar')
+function openbar(){
+  if(sldierbar.style.display==='none'){
+    sldierbar.style.display='block'
+  }else{
+    sldierbar.style.display="none"
+  }
+}
