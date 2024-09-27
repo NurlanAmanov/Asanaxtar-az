3e44894db623f00ee169cc5863c479ba9ea922cc
unknown <nurlanemenov14@gmail.com>
2024-09-27 12:18:23 +0400

    first commit

--- js/app.js
+++ js/app.js
@@ -6,9 +6,20 @@ function openbar(){
     sldierbar.style.display="none"
   }
 }
-document.getElementById('searchButton').addEventListener('click', function() {
-  const query = document.getElementById('searchInput').value;
-  const formattedQuery = `intitle:${query} filetype:pdf`;
-  const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(formattedQuery)}`;
+document.getElementById('searchButton').addEventListener('click', function () {
+  const searchQuery = document.getElementById('searchInput').value.toLowerCase();
+    const checkboxes = document.querySelectorAll('.format-checkbox:checked');
+  let selectedFormats = Array.from(checkboxes).map(checkbox => checkbox.value);
+  
+  
+  let formatQuery = '';
+  if (selectedFormats.length > 0) {
+      formatQuery = ` filetype:${selectedFormats[0]}`; 
+  }
+
+
+  const googleSearchUrl = `https://www.google.com/search?q=intitle:${encodeURIComponent(searchQuery)}${formatQuery}`;
+  
+
   window.open(googleSearchUrl, '_blank');
 });
